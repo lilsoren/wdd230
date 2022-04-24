b5f7faef78fb6828651da1adfef24797bef15103
lilsoren <lily.m.sorensen@gmail.com>
2022-04-23 22:14:31 -0600

    update

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var lm = document.querySelector("#lastmod").textContent = document.lastModified;
+
+var dt = new Date();
+document.querySelector('#date').textContent=dt.getFullYear();
+console.log("this far");

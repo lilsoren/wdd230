9a00e0fb6c12ff0ec3c6a19cb4afeb217cb1bcbd
lilsoren <lily.m.sorensen@gmail.com>
2022-05-30 23:23:46 -0600

    home page continued

--- chamber/js/navigation.js
+++ chamber/js/navigation.js
@@ -1,6 +1,4 @@
-console.log('Made it')
 function toggleMenu(){
-    console.log('toggle works');
     document.getElementById("navigation").classList.toggle("open");
     document.getElementById("hamburger").classList.toggle("open");
 }
@@ -12,6 +10,7 @@ x.onclick = toggleMenu;
 
 // Current date 
 const datefield = document.querySelector(".date");
+const meetingfield = document.querySelector(".meeting-notice");
  // for european/family history format with day first.
 
 // derive the current date using a date object
@@ -19,13 +18,15 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
+//Come join us for the chamber meet and greet Wednesday at 7:00 p.m.
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-
+if (now.getDay() == 2 || now.getDay() == 3){
+    datefield.innerHTML = `<em>${fulldate}</em>` + "<br/>Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
 
 // Last modified and copyright
 var lm = document.querySelector("#lastmod").textContent = document.lastModified;
 
 var dt = new Date();
 document.querySelector('#date').textContent=dt.getFullYear();
-console.log("this far");

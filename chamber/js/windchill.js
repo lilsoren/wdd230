94c6b4e0b8f88f01272f64b1188050de1fd62324
lilsoren <lily.m.sorensen@gmail.com>
2022-06-04 16:04:47 -0600

    added wind chill

---
+++ chamber/js/windchill.js
@@ -0,0 +1,20 @@
+//use the wind chill function 
+let speed = 30;
+let temp = 72;
+builtWC(speed, temp);
+
+
+
+function builtWC(speed, temp){
+    //compute the windchill
+    let wc = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+
+    //round answer down to nearest whole number
+    wc = Math.floor(wc);
+
+    //if chill is greater than temp, just return the temp
+    wc = (wc > temp) ? temp : wc;
+
+    //display the windchill
+    feelTemp.innerHTML = "Feels like: " + wc + "º";
+}

ac5cfe8abfc73f6defcb33670a185b293967def8
lilsoren <lily.m.sorensen@gmail.com>
2022-06-25 21:42:21 -0600

    fixed image names

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -20,6 +20,7 @@ fetch(url)
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     const desc = data.weather[0].description;
 
+    // weather icon
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
